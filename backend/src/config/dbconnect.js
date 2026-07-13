2a16d60a456f5d39a60adf79d3c0e46f702fd815
Waffle47 <wafulanelson2004@gmail.com>
2026-07-14 00:53:27 +0300

    Added DB_PORT to database config for Aiven cloud connection

--- backend/src/config/dbconnect.js
+++ backend/src/config/dbconnect.js
@@ -1,9 +1,10 @@
 //to connect to the db
-const mysql = require ('mysql2');
-require('dotenv').config(); 
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -15,4 +16,3 @@ const pool = mysql.createPool({
 const promisePool = pool.promise();
 
 module.exports = promisePool;
- 
